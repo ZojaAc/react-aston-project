1fafbeb624cf9a62660819212608f461ecd2a5de
Zoja <zan130208@ya.ru>
2022-08-01 04:57:37 +0300

    add notes for form

--- src/containers/FormTemplate.jsx
+++ src/containers/FormTemplate.jsx
@@ -27,11 +27,11 @@ const FormTemplate = ({ title, handleClick }) => {
         />
       </div>
       <div className="form__box">
-        <button className="form__btn btn"
+        <input className="form__btn btn"
+          placeholder={title}          
           onClick={() => handleClick(login, password)}
-        >
-          {title}
-        </button>
+        >          
+        </input>
       </div>      
     </form>
   )
